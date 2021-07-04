8e4b92a6664a4df162a1fae5b00aea5fca637471
b45i <amalshajan2011@gmail.com>
2021-07-04 11:55:42 +0530

    libraries and navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <div className="navbar d-flex justify-content-center bg-gray">
+            <img
+                className="navbar-img"
+                src="https://cdn.worldvectorlogo.com/logos/spacex-logo-1.svg"
+                alt="SpaceX"
+            />
+        </div>
+    );
+};
+
+export default Navbar;
