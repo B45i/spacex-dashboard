276b43b803c42b828bdfe2d677f16ed0879a31c1
b45i <amalshajan2011@gmail.com>
2021-07-04 21:27:21 +0530

    rounded table

--- src/components/LaunchGrid/LaunchTable.jsx
+++ src/components/LaunchGrid/LaunchTable.jsx
@@ -1,38 +1,40 @@
 const LaunchTable = ({ docs }) => {
     return (
-        <table className="table table-borderless">
-            <thead>
-                <tr>
-                    <th>No.</th>
-                    <th>Launched (UTC)</th>
-                    <th>Location</th>
-                    <th>Mission</th>
-                    <th>Orbit</th>
-                    <th>Launch Status</th>
-                    <th>Rocket</th>
-                </tr>
-            </thead>
-            <tbody>
-                {(docs || []).map(i => (
-                    <tr key={i.flight_number}>
-                        <td>
-                            {i.flight_number < 10 ? '0' : ''}
-                            {i.flight_number}
-                        </td>
-                        <td>{i.date_utc}</td>
-                        <td>{i.launchpad_ame}</td>
-                        <td>{i.mission_name}</td>
-                        <td>{i.orbit}</td>
-                        <td>
-                            <span className={`launch-status ${i.status}`}>
-                                {i.status}
-                            </span>
-                        </td>
-                        <td>{i.rocket_name}</td>
+        <div className="table-container">
+            <table className="table table-borderless">
+                <thead>
+                    <tr>
+                        <th>No.</th>
+                        <th>Launched (UTC)</th>
+                        <th>Location</th>
+                        <th>Mission</th>
+                        <th>Orbit</th>
+                        <th>Launch Status</th>
+                        <th>Rocket</th>
                     </tr>
-                ))}
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    {(docs || []).map(i => (
+                        <tr key={i.flight_number}>
+                            <td>
+                                {i.flight_number < 10 ? '0' : ''}
+                                {i.flight_number}
+                            </td>
+                            <td>{i.date_utc}</td>
+                            <td>{i.launchpad_ame}</td>
+                            <td>{i.mission_name}</td>
+                            <td>{i.orbit}</td>
+                            <td>
+                                <span className={`launch-status ${i.status}`}>
+                                    {i.status}
+                                </span>
+                            </td>
+                            <td>{i.rocket_name}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
 };
 
